6f9f7201938aecc5e898c06f01d7796843750b7a
Andrea <andrea.bortolossi89@gmail.com>
2020-04-16 11:28:12 +0200

    fix

--- onboardVMs.sh
+++ onboardVMs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for server in `cat server.txt`;
 do
-    ssh-keyscan -H myadmin@$server >> ~/.ssh/known_hosts
+    ssh-keyscan -H $server >> ~/.ssh/known_hosts
     sshpass -p "Password1234!" ssh-copy-id -i ~/.ssh/id_rsa.pub myadmin@$server
 done
