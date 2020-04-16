952982c0fcb5effadb54eb1dda990906dfc67f62
Andrea <andrea.bortolossi89@gmail.com>
2020-04-16 20:09:56 +0200

    fix

--- activateAZURE.sh
+++ activateAZURE.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Setting environment variables for Terraform to use AZURE"
+echo "Setting environment variables for Ansible to use AZURE"
 export AZURE_SUBSCRIPTION_ID=de2dd9f0-a856-4177-b9f8-9fe12d786b1a
 export AZURE_CLIENT_ID=f152ae7e-5da0-49ca-97dc-8d2e16617e85
 export AZURE_SECRET=7ec68b63-7f52-4980-88f5-17b780cfbfc8
