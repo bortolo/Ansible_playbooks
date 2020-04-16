7bef4a1a06f2da115a0d7a976b162798c0d2138f
Andrea <andrea.bortolossi89@gmail.com>
2020-04-16 20:01:06 +0200

    fix

---
+++ activateAZURE.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Setting environment variables for Terraform to use AZURE"
+export AZURE_SUBSCRIPTION_ID=de2dd9f0-a856-4177-b9f8-9fe12d786b1a
+export AZURE_CLIENT_ID=f152ae7e-5da0-49ca-97dc-8d2e16617e85
+export AZURE_SECRET=7ec68b63-7f52-4980-88f5-17b780cfbfc8
+export AZURE_TENANT=7ea682b6-6e2a-4f9a-b4f4-9bfff0782f62
