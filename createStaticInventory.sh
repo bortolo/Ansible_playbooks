0f8b365340bff9e8444e6cb65817b77ea97c0701
Andrea <andrea.bortolossi89@gmail.com>
2020-04-17 11:17:04 +0200

    create static inventory

--- createStaticInventory.sh
+++ createStaticInventory.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-ansible-inventory -i myazure_rm.yml --list >> inventory.json
-
-LENGTH=jq . tag_environment_app.hosts inventory.json
-ID=0
-echo $LENGTH
-for (( i=0 ; i < $LENGTH , i++ ));
+rm -f server.txt
+LENGTH=$(ansible-inventory -i myazure_rm.yml --list | jq '.tag_environment_app.hosts | length')
+for (( i=0 ; i < $LENGTH ; i++ ));
 do
-  ID=jq . tag_environment_app.hosts[$i] inventory.json
-  echo jq . _meta.hostvars.$ID.ansible_host inventory.json
+  temp=$(ansible-inventory -i myazure_rm.yml --list | jq .tag_environment_app.hosts[$i])
+  temp="${temp%\"}"
+  temp="${temp#\"}"
+  temp=$(ansible-inventory -i myazure_rm.yml --list | jq ._meta.hostvars.$temp.ansible_host)
+  temp="${temp%\"}"
+  temp="${temp#\"}"
+  echo $temp >> server.txt
+done
+LENGTH=$(ansible-inventory -i myazure_rm.yml --list | jq '.tag_environment_web.hosts | length')
+for (( i=0 ; i < $LENGTH ; i++ ));
+do
+  temp=$(ansible-inventory -i myazure_rm.yml --list | jq .tag_environment_web.hosts[$i])
+  temp="${temp%\"}"
+  temp="${temp#\"}"
+  temp=$(ansible-inventory -i myazure_rm.yml --list | jq ._meta.hostvars.$temp.ansible_host)
+  temp="${temp%\"}"
+  temp="${temp#\"}"
+  echo $temp >> server.txt
 done
